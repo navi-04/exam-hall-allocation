8f2a31fcd5f27ee4f5e9020370909baddddc040a
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-02-18 20:45:24 +0530

    small change just for teaching
    small change just for teaching

--- back-end/server.py
+++ back-end/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd 
-from search_module import search_and_display_halls
+# from search_module import search_and_display_halls
 app = Flask(__name__)
 CORS(app)
 
